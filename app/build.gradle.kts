012c22140ea1653d00733e72b85a78b49d311179
SaulH <saulalamillah@gmail.com>
2023-12-15 10:32:48 -0500

    permission camera management

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,8 @@ android {
 dependencies {
 
     val navVersion = "2.7.5"
+    val cameraVersion = "1.2.3"
+
     //Nav component
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
@@ -67,6 +69,13 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.3.1")
+
+    //Camera X
+    implementation ("androidx.camera:camera-core:${cameraVersion}")
+    implementation ("androidx.camera:camera-camera2:${cameraVersion}")
+    implementation ("androidx.camera:camera-lifecycle:${cameraVersion}")
+    implementation ("androidx.camera:camera-view:${cameraVersion}")
+    implementation ("androidx.camera:camera-extensions:${cameraVersion}")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
